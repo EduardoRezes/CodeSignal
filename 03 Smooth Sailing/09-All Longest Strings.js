24322afbd795bc5b7b3844438215dccc0d26160c
Eduardo Rezes <55606483+EduardoRezes@users.noreply.github.com>
2024-01-11 09:27:19 -0300

    My longest String v2

--- 03 Smooth Sailing/09-All Longest Strings.js
+++ 03 Smooth Sailing/09-All Longest Strings.js
@@ -1,24 +1,8 @@
 /*Given an array of strings, return another array containing all of its longest strings.*/
 
 function solution(inputArray) {
-    let arr = inputArray;
-    let longest = 0;
-    let longestArr = [];
-
-    // Iterate through array to find longest length string(s)
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i].length >= longest) {
-            longest = arr[i].length;
-        }
-    }
-
-    // Compare each element in arr to see if it's equal to longest
-    // Push the longest words into longestArr array
-    for(let j = 0; j < arr.length; j++) {
-        if(arr[j].length === longest) {
-            longestArr.push(arr[j]);
-        }
-    }
-
-    return longestArr;
+    //calculates the maximum length of strings in the array.
+    const maxLength = Math.max(...inputArray.map(str => str.length));
+    //filters strings that have a length equal to the maximum length.
+    return inputArray.filter(str => str.length === maxLength);
 }
