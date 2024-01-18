a831b18a0c9634c04248806758e10ebe61b057fb
Eduardo Rezes <55606483+EduardoRezes@users.noreply.github.com>
2024-01-18 14:28:34 -0300

    20 exercise

---
+++ 05 Island of Knowledge/20-arrayMaximalAdjacentDifference.js
@@ -0,0 +1,13 @@
+/**
+ * Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+ */
+
+function solution(inputArray) {
+    var max = Math.abs(inputArray[1] - inputArray[0]);
+    for(var i=2; i < inputArray.length; i++) {
+        if(Math.abs(inputArray[i] - inputArray[i-1]) > max) {
+            max = Math.abs(inputArray[i] - inputArray[i-1]);
+        }
+    }
+    return max
+}
