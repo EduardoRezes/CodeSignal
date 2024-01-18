a1e668fc896deaf38712c66d95f03aeb49eb3dd5
Eduardo Rezes <55606483+EduardoRezes@users.noreply.github.com>
2024-01-18 14:35:36 -0300

    rollback code exercise 20

--- 05 Island of Knowledge/20-arrayMaximalAdjacentDifference.js
+++ 05 Island of Knowledge/20-arrayMaximalAdjacentDifference.js
@@ -3,5 +3,12 @@
  */
 
 function solution(inputArray) {
-    return Math.max(...inputArray.map((num, index) => Math.abs(num - (inputArray[index - 1] || num))));
+    //return Math.max(...inputArray.map((num, index) => Math.abs(num - (inputArray[index - 1] || num))));
+    var max = Math.abs(inputArray[1] - inputArray[0]);
+    for(var i=2; i < inputArray.length; i++) {
+        if(Math.abs(inputArray[i] - inputArray[i-1]) > max) {
+            max = Math.abs(inputArray[i] - inputArray[i-1]);
+        }
+    }
+    return max
 }
